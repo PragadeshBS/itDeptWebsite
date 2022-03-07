2083c006c357fd0141506c5b5776eeeb398d64c7
shankuroko <shanakarsar9942@gmail.com>
2022-03-07 19:43:31 +0530

    first commit

---
+++ admin/faculty.js
@@ -0,0 +1,92 @@
+var faculty = {};
+var columns = [];
+function initData() {
+	document.getElementById("name").value = faculty.name;
+	document.getElementById("designation").value = faculty.designation;
+	document.getElementById("email").value = faculty.emailID;
+	document.getElementById("phone").value = faculty.phoneNumber;
+	document.getElementById("profilePic").src = "." + faculty.image+"?t="+new Date().getTime();
+	document.getElementById("acoe").value = faculty.acoeSiteLink;
+	document.getElementById("personal-site").value = faculty.personalSiteLink;
+	columns=columns.filter((value)=>{return !(value in faculty.details)});
+}
+function updatePrimary() {
+	//formData.append("id",ID);
+	var formData = new FormData();
+	var img = $("#image").prop("files")[0];
+	formData.append("image", img);
+	formData.append("fid", ID);
+	formData.append("name",$("#name").val());
+	formData.append("designation", $("#designation").val());
+	formData.append("email", $("#email").val());
+	formData.append("phoneNumber", $("#phone").val());
+	formData.append("acoeSite", $("#acoe").val());
+	formData.append("personalSite", $("#personal-site").val());
+
+	$.ajax({
+		type: "POST",
+		url: "../PHP/updatePrimaryFaculty.php",
+		data: formData,
+		processData: false,
+		contentType: false,
+		complete: (d) => {
+			console.log(d.responseText);
+			$("#profilePic").src = $("#profilePic").src +"?t="+new Date().getTime();
+			$("#image").val(null);
+			if (d.responseText.trim() == "true") {
+				document.getElementById("updateMsg-P").style["transform"] = "scale(1)";
+				setTimeout(() => {
+					document.getElementById("updateMsg-P").style["transform"] =
+						"scale(0)";
+				}, 1500);
+			}
+		},
+	});
+}
+window.onload = () => {
+	$(
+		$.ajax({
+			type: "POST",
+			url: "../PHP/getFaculty.php",
+			data: {
+				fid: 0,
+			},
+			complete: (d) => {
+				try {
+					let data = JSON.parse(d.responseText);
+					columns = data.details.split("#");
+				} catch (e) {
+					alert("Counld Not Find Columns");
+					window.loaction = "/";
+				}
+			},
+		})
+	);
+	$(
+		$.ajax({
+			type: "POST",
+			url: "../PHP/getFaculty.php",
+
+			data: {
+				fid: ID,
+			},
+			complete: (d) => {
+				try {
+					faculty = JSON.parse(d.responseText.replace(/\n/g, ""));
+					if(faculty.details==null)
+						faculty.details=[];
+					initData();
+				} catch (e) {
+					alert("Faculty Not Found!!!");
+					window.location = "/";
+				}
+			},
+		})
+	);
+	$("#image").change(function(){
+		if ($(this).val().split(".").pop() != "webp") {
+			$(this).val(null);
+			alert("Image must be of 'webp' type");
+		}
+	});
+};
---
+++ admin/faculty.php
@@ -0,0 +1,76 @@
+<?php 
+  if(!isset($_GET['id'])){
+    echo '<script language="javascript">';
+    echo 'alert("Unauthorized Access!!Please Login");';
+    echo 'window.location="/"';
+    echo '</script>';
+  }
+  echo '<script>const ID ='.$_GET["id"].';</script>';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./admin.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="./faculty.js" crossorigin="anonymous"></script>
+</head>
+<body>
+  <header>
+    <img src="../Images/annaUniv.svg" alt="">
+    <div class="heading">
+    <div class="heading-title">Department of Information Technology</div>
+    <div class="heading-dept">Anna University , MIT Campus</div>
+    </div>
+    <a href="/" tooltip="logout"><i class="bi bi-box-arrow-right"></i> <span>Logout</span></a> 
+  </header>
+  <div class="intro-container" id="primaryForm">
+    <img src="?m=' . filemtime('image.jpg') . '" alt="" id="profilePic">
+    <label for="name">Name :</label>
+    <input type="text" name="name" id="name">   
+    <label for="designation">Designation :</label>
+    <input type="text" name="designation" id="designation">
+    <label for="email">Email ID :</label>
+    <input type="text" name="email" id="email">
+    <label for="phone">Phone Number :</label>
+    <input type="text" name="phone" id="phone">
+    <label for="acoe">ACOE Site :</label>
+    <input type="text" name="acoe" id="acoe">
+    <label for="personal-site">Personal Site :</label>
+    <input type="text" name="personal-site" id="personal-site">
+    <label for="image">Profile Image :</label>
+    <input type="file" name="image" id="image">
+    <button id="primaryUpd" onclick="updatePrimary()">
+      Update Information
+      <label  id="updateMsg-P">Basic Information Update Completed !!</label>
+    </button>        
+  </div>
+  <h1>Fields</h1>
+  <div class="detail-option">
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">information</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+    <li onclick="getInfo()">Hello</li>
+  </div>
+  <div class="faculty-details">
+    
+    
+  </div>  
+</body>
+</html>
