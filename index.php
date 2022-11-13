c6698f197655380c769d26423a44f776b1ffe6af
Pragadesh BS <pragadeshbs@protonmail.com>
2022-11-13 10:41:20 +0530

    add slide on homepage

--- index.php
+++ index.php
@@ -22,11 +22,9 @@
             The department has a team of dynamic, young and dedicated staff, with specialization in wide array of areas.
             It offers full time and part time courses for both UG and PG. The department receives funds from DST under
             FIST programme.
-            <br><br>All the members of TEAM IT share the belief that IT is a tool, if used effectively can improve the
-            quality of all activities being carried out. As a first step, a project to automate all administrative
-            activities has been taken up and the entire project is being done using the services of our department
-            staffs. In the near future the department will have post Graduate course (other than MS By Research)
-            offering curriculum in the field of Computer Science and Information Technology.
+            <br><br>A new course, Artificial Intelligence &amp; Data Science, has been introduced that keeps the
+            students
+            in line with the current trends of technology.
             <br><br>Department of Information Technology, MIT, thrives to accomplish its vision of developing dexterous
             and proficient students. Its progress is growing exponentially in contributing efficient and technically
             sound students to the corporate world.
@@ -245,5 +243,12 @@ $("#next-poster").click(() => {
 $(document).ready(() => {
     $("#prev-poster").hide();
 })
+
+setInterval(() => {
+    let min = Math.ceil(1);
+    let max = Math.floor(15);
+    let cur = Math.floor(Math.random() * (max - min + 1)) + min;
+    $(".about-us-bg").css("background-image", `url(Images/gallery/${cur}.png)`)
+}, 3000)
 </script>
 <?php require("templates/footer.php"); ?>
