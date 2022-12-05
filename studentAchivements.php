54e343aa41887afe1df00e3744f97b1d0960ed7a
shankuroko <shankarsar9942@gmail.com>
2022-12-05 16:25:17 +0530

    phd fix

--- studentAchivements.php
+++ studentAchivements.php
@@ -2,19 +2,26 @@
 <div class="body-start"></div>
 <h1 class="mt-5">Student Achievements</h1>
 <div class="card studentAwards mb-2">
-    <img src="./Images/studentAwards/1.webp" alt="">
-    <img src="./Images/studentAwards/2.webp"  alt="">
-    <img src="./Images/studentAwards/3.webp" alt="">
-    <img src="./Images/studentAwards/4.webp" alt="">
-    <img src="./Images/studentAwards/5.webp" class="img-c2" alt="">
-    <img src="./Images/studentAwards/6.webp" alt="">    
-    <img src="./Images/studentAwards/12.webp" alt="">
-    <img src="./Images/studentAwards/7.webp" alt="">
-    <img src="./Images/studentAwards/8.webp" alt=""> 
-    <img src="./Images/studentAwards/9.webp" alt="">
-    <img src="./Images/studentAwards/10.webp" alt="">
-    <img src="./Images/studentAwards/11.webp" alt="">   
-    <img src="./Images/studentAwards/13.webp" class="img-c2" alt="">
+    <img src="./Images/studentAwards/1.webp" alt="" loading="lazy">
+    <img src="./Images/studentAwards/15.webp" alt="" loading="lazy">
+    <img src="./Images/studentAwards/16.webp" alt="" loading="lazy">
+    <img src="./Images/studentAwards/2.webp"  alt="" loading="lazy">
+    <img src="./Images/studentAwards/3.webp" alt="" loading="lazy">
+    <img src="./Images/studentAwards/4.webp" alt="" loading="lazy">
+    <img src="./Images/studentAwards/5.webp" class="img-c2" alt="" loading="lazy">
+    <img src="./Images/studentAwards/6.webp" alt="" loading="lazy">    
+    <img src="./Images/studentAwards/12.webp" alt="" loading="lazy">
+    <img src="./Images/studentAwards/7.webp" alt="" loading="lazy">
+    <img src="./Images/studentAwards/8.webp" alt="" loading="lazy">    
+    <img src="./Images/studentAwards/20.webp" class="img-c2" alt="" loading="lazy"> 
+    <img src="./Images/studentAwards/9.webp" alt="" loading="lazy">    
+    <img src="./Images/studentAwards/17.webp" alt="" loading="lazy">
+    <img src="./Images/studentAwards/18.webp" alt="" loading="lazy">
+    <img src="./Images/studentAwards/10.webp" alt="" loading="lazy">
+    <img src="./Images/studentAwards/11.webp" alt="" loading="lazy">   
+    <img src="./Images/studentAwards/13.webp" class="img-c2" alt="" loading="lazy"> 
+    <img src="./Images/studentAwards/14.webp" alt="" loading="lazy">     
+    <img src="./Images/studentAwards/19.webp" alt="" loading="lazy">
 </div>
 <style>
 thead {
